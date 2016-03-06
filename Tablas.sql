7d61a2e6f0897faaaca867ecbc86c5ee0d1c5b39
enriqueBC <enrique_bernal@ciencias.unam.mx>
2016-03-06 02:19:33 -0600

    Comentarios en scripts
    Se agregaron los comentarios requeridos utilizando el comando COMMENT en
    las tablas creadas para la base de datos.

--- Tablas.sql
+++ Tablas.sql
@@ -1,7 +1,16 @@
+COMMENT ON TABLE Usuario IS
+'Tabla para almacenar la informacion de los usuarios
+Contiene un correo como identificador,
+y un nombre de usuario';
+
 CREATE TABLE Usuario (
 	correo			VARCHAR(200) PRIMARY KEY,
 	nombreUsuario	VARCHAR(100)
 );
+
+COMMENT ON TABLE Pais IS
+'Tabla para almacenar los nombres de paises preveniendo redundancia
+Contiene el nombre del pais a guardar';
 
 CREATE TABLE Pais (
 	nombrePais	VARCHAR(200) PRIMARY KEY
